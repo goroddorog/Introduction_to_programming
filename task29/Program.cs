96c203929ed075c2e0e5943d6d200d6a09682483
zezyla <zezyla@gmail.com>
2022-12-10 12:23:23 +0300

    Added Task34. Edit Task29

--- task29/Program.cs
+++ task29/Program.cs
@@ -2,47 +2,47 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-// int ReadArray(int count)
-// {
-//     int[] array = new int[count];
-//     for (int i = 0; i < count; i++)
-//     {
-//         Console.WriteLine($"Введите значение {i+1} элемента массива:");
-//         array[i] = Convert.ToInt32(Console.ReadLine());
-//     }
-//     return array;
-// }
+int[] ReadArray(int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"Введите значение {i+1} элемента массива:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
 
-// void WriteArray(int count, int[] array)
-// {
-//     Console.Write("Массив: ");
-//     for (int i = 0; i < count; i++)
-//     {
-//         Console.Write(array[i]);
-//     }
-// }
-
-
-// Console.WriteLine("Введите количество элементов массива:");
-// int count = Convert.ToInt32(Console.ReadLine());
-
-// int[] array = ReadArray(count);
-// WriteArray(count, array);
+void WriteArray(int count, int[] array)
+{
+    Console.Write("Массив: ");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
 
 
 Console.WriteLine("Введите количество элементов массива:");
 int count = Convert.ToInt32(Console.ReadLine());
 
-int[] array = new int[count];
-for (int i = 0; i < count; i++)
-{
-    Console.WriteLine($"Введите значение {i+1} элемента массива:");
-    array[i] = Convert.ToInt32(Console.ReadLine());
-}
+int[] array = ReadArray(count);
+WriteArray(count, array);
 
-Console.Write("Массив:");
-for (int i = 0; i < count; i++)
-{
-    Console.Write($" {array[i]}");
-}
+
+// Console.WriteLine("Введите количество элементов массива:");
+// int count = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = new int[count];
+// for (int i = 0; i < count; i++)
+// {
+//     Console.WriteLine($"Введите значение {i+1} элемента массива:");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// Console.Write("Массив:");
+// for (int i = 0; i < count; i++)
+// {
+//     Console.Write($" {array[i]}");
+// }
 
---
+++ task34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int ReadArray(int length)
+{
+    int[] array = new int[length];
+    int count_number = 0;
+    Console.WriteLine("Элементы массива: ");
+    for (int i = 0; i < length; i++)
+    {
+        
+        array[i] = new Random().Next(100,1000);
+        if (array[i] % 2 == 0)
+            {
+                count_number += 1;
+            }
+        Console.Write($"{array[i]} ");
+    }
+    return count_number;
+}
+
+Console.WriteLine("Введите количество элементов массива:");
+int array_length = Convert.ToInt32(Console.ReadLine());
+
+int even_number = ReadArray(array_length);
+Console.WriteLine($"\nКоличество четных чисел: {even_number}");
