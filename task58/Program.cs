5623fd9ea045d4e7b18ec5a718f2eb4e295b325d
zezyla <zezyla@gmail.com>
2022-12-20 21:27:39 +0300

    Added task58

--- task58/Program.cs
+++ task58/Program.cs
@@ -6,3 +6,60 @@
 // 18 20
 // 15 18
 
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    Random random = new Random();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = random.Next(0, 9);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiArray(int[,] array1, int[,] array2)
+{
+    int[,] multiMatrix = new int[array1.GetLength(0), array1.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            for (int k = 0; k < array2.GetLength(0); k++)
+            {
+                // for (int m = 0; m < array2.GetLength(1); m++)
+                // {
+                    multiMatrix[i, j] += array1[i, k] * array2[k, j];
+                // }
+            }
+        }
+    }
+    return multiMatrix;
+}
+
+Console.WriteLine("Введите размер матрицы:");
+int row = Convert.ToInt32(Console.ReadLine());
+int col = row;
+
+int[,] matrix1 = GenerateArray(row, col);
+int[,] matrix2 = GenerateArray(row, col);
+Console.WriteLine("Исходные массивы:");
+PrintArray(matrix1);
+Console.WriteLine($"");
+PrintArray(matrix2);
+Console.WriteLine($"Произведение двух матриц:");
+PrintArray(MultiArray(matrix1, matrix2));
