d2271911a6289cdedbb727657e738f7a841afa23
zezyla <zezyla@gmail.com>
2022-12-24 13:56:45 +0300

    Edit task62

--- task62/Program.cs
+++ task62/Program.cs
@@ -10,43 +10,6 @@
 // PrintArray(matrix);
 
 
-// //  Функция заполнения массива по спирали начиная с 1
-// void FillArraySpiral(int[,] array, int size)
-// {
-//     int i = 0, j = 0;
-//     int value = 1;
-//     for (int e = 0; e < size * size; e++)
-//     {
-//         int k = 0;
-//         do { array[i, j++] = value++; } while (++k < size - 1);
-//         for (k = 0; k < size - 1; k++) array[i++, j] = value++;
-//         for (k = 0; k < size - 1; k++) array[i, j--] = value++;
-//         for (k = 0; k < size - 1; k++) array[i--, j] = value++;
-//         ++i; ++j;
-//         if (size < 2) size = 0;
-//         else size = size -2;
-//         // size = size < 2 ? 0 : size - 2;
-//     }
-// }
-
-// //  Функция вывода двумерного массива в терминал
-// void PrintArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             if (array[i, j] < 10)
-//             {
-//                 Console.Write("0" + array[i, j]);
-//                 Console.Write(" ");
-//             }
-//             else Console.Write(array[i, j] + " ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
 int matrixSize = 4;
 matrixSize += 2;
 int[,] matrix = new int[matrixSize, matrixSize];
@@ -54,6 +17,7 @@ int count = 1;
 MakeBorder(matrix, matrixSize);
 FillArraySpiral(1, 1);
 Console.WriteLine();
+
 
 MakeBorder(matrix, matrixSize);
 
